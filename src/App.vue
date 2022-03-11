057cea1a653812128bcef10e7b0ac688ae4acbc1
Andu15 <estefania_8_3@hotmail.com>
2022-03-11 02:19:24 -0500

    add tailwind configurations and route navigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
-<script setup>
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/main">Main</router-link> |
+    <router-link to="/positions">Positions</router-link> |
+    <router-link to="/invoice">Invoice</router-link> 
+  </div>
+  <router-view/>
+</template>
+
+<script >
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
 
 <style>
 #app {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,24 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        "fluorescent-green": "#98F5E1",
+        "medium-green": "#7DDCD3",
+        "aqua-green": "#CBF3F0"
+      },
+      secondary: {
+        "pasty-green": "#2EC4B6"
+      },
+      neutral: {
+        "greenish-lead": "#B3C7C5",
+        "soft-white": "#F9F9F9"
+      }
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
