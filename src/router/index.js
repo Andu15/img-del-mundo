444ce33172d44e61408b4a8e9e9bb173f354c24d
Andu15 <estefania_8_3@hotmail.com>
2022-03-11 01:42:28 -0500

    feat: add project structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: () => import(/* webpackChunkName: "main" */ '../views/Main.vue')
+  },
+  {
+    path: '/positions',
+    name: 'Positions',
+    component: () => import(/* webpackChunkName: "positions" */ '../views/Positions.vue')
+  },
+  {
+    path: '/invoice',
+    name: 'Invoice',
+    component: () => import(/* webpackChunkName: "invoice" */ '../views/Invoice.vue')
   }
 ]
 
